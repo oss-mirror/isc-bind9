2945bf499974d95851e1dcaad701adc85737ff95
Michal Nowak <mnowak@isc.org>
2020-12-14 19:12:56 +0100

    Remove sequential test execution support from runall.sh
    We don't have any sequential system tests anymore, support of sequential
    test execution in the runall.sh script is therefore not needed.

--- bin/tests/system/runall.sh
+++ bin/tests/system/runall.sh
@@ -24,17 +24,16 @@
 #               (This is ignored when running on Windows as the tests are always
 #               run sequentially on that platform.)
 
+# shellcheck source=conf.sh
 . ./conf.sh
 
 usage="Usage: ./runall.sh [-c] [-n] [numprocesses]"
 
 # Preserve values of environment variables which are already set.
-
 SYSTEMTEST_FORCE_COLOR=${SYSTEMTEST_FORCE_COLOR:-0}
 SYSTEMTEST_NO_CLEAN=${SYSTEMTEST_NO_CLEAN:-0}
 
 # Handle command line switches if present.
-
 while getopts "cn-" flag; do
     case "$flag" in
         c) SYSTEMTEST_FORCE_COLOR=1 ;;
@@ -47,7 +46,6 @@ export NOCLEAN
 shift $((OPTIND-1))
 
 # Obtain number of processes to use.
-
 if [ $# -eq 0 ]; then
     numproc=1
 elif [ $# -eq 1 ]; then
@@ -63,46 +61,23 @@ else
 fi
 
 # Run the tests.
-
 export SYSTEMTEST_FORCE_COLOR
 export SYSTEMTEST_NO_CLEAN
 
 status=0
 
-if [ "$NOPARALLEL" = "" ]; then
-    if [ "$CYGWIN" = "" ]; then
-        # Running on Unix, use "make" to run tests in parallel.
-        make -j "$numproc" check
-        status=$?
-    else
-        # Running on Windows: Cygwin "make" is available, but isn't being
-        # used for the build. So we create a special makefile for the purpose
-        # of parallel execution of system tests, and use that.
-        $SHELL parallel.sh > parallel.mk
-        make -f parallel.mk -j "$numproc" check
-        $SHELL ./testsummary.sh || status=1
-    fi
+if [ "$CYGWIN" ]; then
+    # Running on Windows: Cygwin "make" is available, but isn't being
+    # used for the build. So we create a special makefile for the purpose
+    # of parallel execution of system tests, and use that.
+    $SHELL parallel.sh > parallel.mk
+    make -f parallel.mk -j "$numproc" check
+    $SHELL testsummary.sh
+    status=$?
 else
-    # the NOPARALLEL environment variable indicates that tests must be
-    # run sequentially.
-    $PERL testsock.pl || {
-        cat <<-EOF
-	I:NOTE: System tests were skipped because they require the
-	I:      test IP addresses 10.53.0.* to be configured as alias
-	I:      addresses on the loopback interface.  Please run
-	I:      "bin/tests/system/ifconfig.sh up" as root to configure them.
-	EOF
-        exit 1
-    }
-    (
-	status=0
-        for testdir in $SUBDIRS; do
-            $SHELL run.sh -r "$testdir" || status=1
-        done
-	echo "$status" > systests.status
-    ) 2>&1 | tee "systests.output"
-    read -r status < systests.status
-    rm systests.status
+    # Running on Unix, use "make" to run tests in parallel.
+    make -j "$numproc" check
+    status=$?
 fi
 
 exit "$status"
