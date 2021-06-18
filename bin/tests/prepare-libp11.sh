b69f28f77639b5cb8d5ea547eb0f3b7cdce4aa5d
Matthijs Mekking <matthijs@isc.org>
2021-06-18 09:47:48 +0200

    fixup! Use the engine_pkcs11 for the softhsm2.4 and softhsm2.6 unit and system tests

--- bin/tests/prepare-libp11.sh
+++ bin/tests/prepare-libp11.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+# OpenSC/libp11
 git clone https://github.com/OpenSC/libp11.git /var/tmp/libp11
 cd /var/tmp/libp11
 ./bootstrap
@@ -7,3 +8,12 @@ cd /var/tmp/libp11
 make
 make install
 cd ../../..
+
+# OpenSC/OpenSC
+git clone git://github.com/OpenSC/OpenSC.git /var/tmp/OpenSC
+cd /var/tmp/OpenSC
+./bootstrap
+./configure --prefix=/usr --sysconfdir=/etc/opensc
+make
+make install
+cd ../../../
