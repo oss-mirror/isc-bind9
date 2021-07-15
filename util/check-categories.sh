siserin/blind9

Log the rcode returned to for a query.

util/check-categories.sh
@@ -25,7 +25,7 @@ do
 	done
 	if test $ok = no
 	then
-		echo "$i missing from documentation."
+		echo "$i missing from doc/arm/logging-categories.xml."
 	fi
 done
 for i in $list2
@@ -40,6 +40,6 @@ do
 	done
 	if test $ok = no
 	then
-		echo "$i not in code."
+		echo "documented logging category '$i' not in code."
 	fi
 done

oss-mirror/isc-bind9

util/check-categories.sh
@@ -33,7 +33,7 @@ do
 	done
 	if test $ok = no
 	then
-		echo "$i missing from documentation."
+		echo "$i missing from doc/arm/logging-categories.rst."
 		status=1
 	fi
 done
@@ -49,7 +49,7 @@ do
 	done
 	if test $ok = no
 	then
-		echo "$i not in code."
+		echo "documented logging category '$i' not in code."
 		status=1
 	fi
 done