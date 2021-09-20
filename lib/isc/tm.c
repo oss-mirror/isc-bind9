6737339965c1dc0c1c0c4199da85e8f8b25bb299
Ondřej Surý <ondrej@isc.org>
2021-09-20 11:17:53 +0200

    More dual licensed files

--- lib/isc/tm.c
+++ lib/isc/tm.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
  *
- * SPDX-License-Identifier: MPL-2.0
+ * SPDX-License-Identifier: MPL-2.0 AND BSD-2-Clause
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
